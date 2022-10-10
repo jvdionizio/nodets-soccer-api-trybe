ec5f09fadd074d2807dcb3c8fe3928771f23a253
jvdionizio <dionizio145@gmail.com>
2022-10-10 15:03:12 -0300

    Feat: migrations and model for matches

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,52 @@
+import { BOOLEAN, INTEGER, Model } from 'sequelize';
+import db from '.';
+import IMatch from '../../interfaces/IMatch';
+import Team from './team';
+
+class Match extends Model implements IMatch {
+  public id!: number;
+  public homeTeam: number;
+  public homeTeamGoals: number;
+  public awayTeam: number;
+  public awayTeamGoals: number;
+  public inProgress: number;
+}
+
+Match.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeam: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'teamHome' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'teamAway' });
+
+export default Match;
